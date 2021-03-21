54e426f58345b1cb6678c5868751a44b395df2a2
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-03-21 17:17:05 +1000

    finish section 1 problem 1

---
+++ Section 1/problem1.cpp
@@ -0,0 +1,58 @@
+/* String Censor
+ *
+ * This program asks for two strings. It prints the first string with all the characters from the second string removed.
+ * */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string getLine(void);
+string stringCensor1(string s1, string s2);
+string stringCensor2(string &s1, string s2);
+
+int main()
+{
+  cout << "Enter a string: ";
+  string text = getLine();
+
+  cout << "Enter letters to remove: ";
+  string censor = getLine();
+
+  string censoredString = stringCensor2(text, censor);
+  cout << censoredString << endl;
+
+  return 0;
+}
+
+string getLine()
+{
+  string s;
+  getline(cin, s);
+  return s;
+}
+
+string stringCensor1(string s1, string s2)
+{
+  string censoredString;
+  for (int i = 0; i < s1.length(); i++)
+  {
+    if (s2.find(s1[i]) == string::npos)
+      censoredString += s1[i];
+  }
+  return censoredString;
+}
+
+string stringCensor2(string &s1, string s2)
+{
+  for (int i = 0; i < s2.length(); i++)
+  {
+    int index = 0;
+    while ((index = s1.find(s2[i], index)) != string::npos)
+    {
+      s1.erase(index, 1);
+    }
+  }
+  return s1;
+}
