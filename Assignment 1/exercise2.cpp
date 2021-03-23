167fd30653ae315c171ee8af66613a340315e499
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-03-23 17:34:32 +1000

    finish Assignment 1 Exercise 2

--- Assignment 1/exercise2.cpp
+++ Assignment 1/exercise2.cpp
@@ -2,10 +2,13 @@
  *
  * This program accepts several parameters related to voting simulation.
  * Given the parameters, it will then simulate 500 outcomes and state the likelihood of an invalid result.
+ * The program assumes that Candidate A is ahead, and therefore calculates an invalid trial when A loses to B.
  * */
 
 #include "../Headers/common.h"
 #include <random>
+
+#define NUMBER_OF_TRIALS 500
 
 int main()
 {
@@ -13,17 +16,50 @@ int main()
   int nVoters;
   double pSpread, pError;
 
-  cout << "Enter the number of voters (positive integer): ";
-  getline(cin, input);
-  nVoters = stoi(input);
+  do
+  {
+    cout << "Enter the number of voters (positive integer): ";
+    getline(cin, input);
+    nVoters = stoi(input);
+  } while (nVoters < 1);
 
-  cout << "Enter the percentage spread between candidates (0.0 to 1.0): ";
-  getline(cin, input);
-  pSpread = stod(input);
+  do
+  {
+    cout << "Enter the percentage spread between candidates (0.0 to 1.0): ";
+    getline(cin, input);
+    pSpread = stod(input);
+  } while (pSpread < 0.0 || pSpread > 1.0);
 
-  cout << "Enter the percentage of voting error (0.0 to 1.0): ";
-  getline(cin, input);
-  pError = stod(input);
+  do
+  {
+    cout << "Enter the percentage of voting error (0.0 to 1.0): ";
+    getline(cin, input);
+    pError = stod(input);
+  } while (pSpread < 0.0 || pSpread > 1.0);
+
+  int nInvalid = 0;
+  for (int i = 0; i < NUMBER_OF_TRIALS; i++)
+  {
+    int votesA = nVoters * (.5 + pSpread / 2.0);
+    int votesB = nVoters * (.5 - pSpread / 2.0);
+
+    int postErrorVotesA = 0, postErrorVotesB = 0;
+
+    for (int vote = 0; vote < votesA; vote++)
+    {
+      double(rand()) / (double(RAND_MAX) + 1.0) > pError ? postErrorVotesA++ : postErrorVotesB++;
+    }
+
+    for (int vote = 0; vote < votesB; vote++)
+    {
+      double(rand()) / (double(RAND_MAX) + 1.0) > pError ? postErrorVotesB++ : postErrorVotesA++;
+    }
+
+    if (postErrorVotesA < postErrorVotesB)
+      nInvalid++;
+  }
+
+  cout << "Chance of Invalid Trial: " << double(nInvalid) / double(NUMBER_OF_TRIALS) * 100.0 << endl;
 
   return 0;
 }
