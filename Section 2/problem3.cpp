332fe8a53db3aa3a6c1b105d48af88de3189459a
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-03-27 19:04:19 +1000

    finish section 2 problem 3

--- Section 2/problem3.cpp
+++ Section 2/problem3.cpp
@@ -1,30 +1,55 @@
 /* Exercise 3: Using the Scanner and Stack Classes
  *
- * This program reads in an HTML file and determines if the HTML is properly nested.
- * ASSUME: The HTML tags are all well-formed and there are no erroneous angle brackets.
- * ALSO: The tags are not self-closing. E.g. No <hr> or <div /> nonsense.
+ * This program reads in an HTML file and determines if the HTML is properly
+ * nested. ASSUME: The HTML tags are all well-formed and there are no erroneous
+ * angle brackets. ALSO: The tags are not self-closing. E.g. No <hr> or <div />
+ * nonsense.
  * */
+
+#include <stack>
 
 #include "../Library/common.h"
 #include "../Library/tokenscanner.h"
-#include <stack>
 
-bool isCorrectlyNested(ifstream &file)
-{
+bool isCorrectlyNested(string htmlString);
 
-  return true;
+int main() {
+  string input;
+  bool result = false;
+
+  cout << "Enter a string: ";
+  getline(cin, input);
+
+  result = isCorrectlyNested(input);
+
+  cout << result << endl;
+  return 0;
 }
 
-int main()
-{
-  ifstream file;
-  do
-  {
-    string filename;
-    cout << "Enter filename: ";
-    getline(cin, filename);
-    file.open(filename);
-  } while (file.fail());
+bool isCorrectlyNested(string htmlString) {
+  TokenScanner scanner;
+  scanner.ignoreWhitespace();
+  scanner.setInput(htmlString);
 
-  return 0;
+  stack<string> stack;
+  bool isTag = false, isClosingTag = false;
+
+  while (scanner.hasMoreTokens()) {
+    string token = scanner.nextToken();
+
+    if (token == "<") {
+      isTag = true;
+    } else if (isTag && token == "/") {
+      isTag = false;
+      isClosingTag = true;
+    } else if (token == ">") {
+      isTag = isClosingTag = false;
+    } else if (isTag) {
+      stack.push(token);
+    } else if (isClosingTag) {
+      if (stack.top() == token) stack.pop();
+    }
+  }
+
+  return stack.empty();
 }
