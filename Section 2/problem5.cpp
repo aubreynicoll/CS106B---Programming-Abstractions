2ecf6dbe0f268f23bb3369b68c3072bb0f9b139d
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-03-31 16:21:58 +1000

    add Grid implementation

---
+++ Section 2/problem5.cpp
@@ -0,0 +1,6 @@
+/* Problem 5: Minesweeper
+ *
+ * This program contains a function that takes a reference to a matrix<bool>,
+ * and returns a matrix<int> where int is the number of true values within a
+ * radius of 1.
+ * */
