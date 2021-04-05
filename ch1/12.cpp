9b45b8c150a2212bc1c4ff7db6d1a00641eeca32
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-04-05 12:59:33 +1000

    finish ch 1 ex 12

---
+++ ch1/12.cpp
@@ -0,0 +1,31 @@
+/* Ch. 1, Exercise 12
+ *
+ * This program computes the area of a quarter circle, radius of 2, by
+ * calculating the area of a series of rectangles whose upper mid-points
+ * intersect the arc of the quarter circle.
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+const int RADIUS = 2;
+const int N_RECTANGLES = 10000;
+
+int main() {
+  cout << "This program prints an approximation of the area of a quarter "
+          "circle, radius "
+       << RADIUS << endl;
+  double width = double(RADIUS) / N_RECTANGLES;
+  double area = 0.0;
+
+  for (int i = 0; i < N_RECTANGLES; i++) {
+    double midpoint = i * width + width / 2;
+    double height = sqrt(RADIUS * RADIUS - midpoint * midpoint);
+
+    area += height * width;
+  }
+
+  cout << "The area is approximately: " << area << endl;
+  return 0;
+}
