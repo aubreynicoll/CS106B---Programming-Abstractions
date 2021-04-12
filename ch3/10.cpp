8e48d6d2b2bda2666b419e11e5f0f2af15dd43ab
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-04-12 14:49:03 +1000

    finish exercise 3.10

--- ch3/10.cpp
+++ ch3/10.cpp
@@ -10,13 +10,14 @@ using namespace std;
 
 string replaceAll(string s, char c1, char c2);
 string replaceAll(string s1, char c, string s2);
+string replaceAll(string s1, string s2, string s3);
 
 int main() {
   cout << "This program takes a string and two characters, replacing each "
           "occurrence of the first character with the second inside the string."
        << endl;
 
-  cout << replaceAll("Hello test", 't', "hello") << endl;
+  cout << replaceAll("poke the pokemon in the wild.", "poke", "pika!") << endl;
   return 0;
 }
 
@@ -32,6 +33,19 @@ string replaceAll(string s1, char c, string s2) {
   for (int i = 0; i < s1.length(); i++) {
     if (s1[i] == c) {
       newString += s2;
+    } else {
+      newString += s1[i];
+    }
+  }
+  return newString;
+}
+
+string replaceAll(string s1, string s2, string s3) {
+  string newString;
+  for (int i = 0; i < s1.length(); i++) {
+    if (s1.substr(i, s2.length()) == s2) {
+      newString += s3;
+      i += s2.length() - 1;
     } else {
       newString += s1[i];
     }
